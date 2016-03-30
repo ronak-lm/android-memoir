5f856e8c494f0b8d8b1f30a3cabc36fe10002165
Ronak Manglani <ronak.manglani@gmail.com>
2016-03-30 23:54:43 +0530

    Create UI for login activity

--- app/src/main/java/com/momana/bhromo/memoir/activity/LoginActivity.java
+++ app/src/main/java/com/momana/bhromo/memoir/activity/LoginActivity.java
@@ -1,27 +1,34 @@
 package com.momana.bhromo.memoir.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 import com.momana.bhromo.memoir.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
 
-    @Bind(R.id.toolbar) Toolbar toolbar;
+    @Bind(R.id.password) EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
-        setSupportActionBar(toolbar);
     }
 
+    @OnClick(R.id.login_button)
+    public void onLoginButtonClicked() {
+        finish();
+        startActivity(new Intent(this, MainActivity.class));
+    }
 }
