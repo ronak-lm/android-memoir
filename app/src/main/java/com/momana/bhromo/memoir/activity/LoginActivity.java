2e88428a4888f9c41d62d2d2b648b85954fce228
Ronak Manglani <ronak.manglani@gmail.com>
2016-03-30 20:47:59 +0530

    Create login activity

---
+++ app/src/main/java/com/momana/bhromo/memoir/activity/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.momana.bhromo.memoir.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.momana.bhromo.memoir.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar) Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+    }
+
+}
